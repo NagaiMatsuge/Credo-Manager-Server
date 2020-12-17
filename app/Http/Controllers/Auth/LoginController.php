bcf42f2625c2db589b76c9da90200d26a603bde1
NagaiMatsuge <komilovboburweb@gmail.com>
2020-12-17 11:49:17 +0500

    Minor fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Mail\VerifyEmail;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Traits\ResponseTrait;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Mail;
 
 class LoginController extends Controller
@@ -51,10 +53,35 @@ class LoginController extends Controller
         //     'working_days' => '[1, 2, 3, 4, 5]',
         //     'color' => '#8F73FC'
         // ]);
-        // return response()->json($user->toArray());
+
+        // $user2 = User::create([
+        //     'name' => 'Palonchi',
+        //     'email' => 'TestEmail@gmail.com',
+        //     'password' => Hash::make('password'),
+        //     'work_start_time' => '9:9:9',
+        //     'work_end_time' => '8:8:8',
+        //     'pause_start_time' => '7:7:7',
+        //     'pause_end_time' => '6:6:6',
+        //     'working_days' => '[1, 2, 3, 4, 5]',
+        //     'color' => '#FCB573'
+        // ]);
+        // $user3 = User::create([
+        //     'name' => 'Somebody',
+        //     'email' => 'TestTestEmail@gmail.com',
+        //     'password' => Hash::make('password'),
+        //     'work_start_time' => '9:9:9',
+        //     'work_end_time' => '8:8:8',
+        //     'pause_start_time' => '7:7:7',
+        //     'pause_end_time' => '6:6:6',
+        //     'working_days' => '[1, 2, 3, 4, 5]',
+        //     'color' => '#FC73AD'
+        // ]);
+        // return response()->json($user2->toArray());
         // $role = Role::create(['name' => 'Admin', 'guard_name' => 'web']);
-        // $user = User::find('81e52ba5-77bf-4e5b-ad86-216fec1a40ca');
-        // $user->syncRoles('Admin');
+        // $user = User::find('290001ce-f3b6-4f93-9e90-eb1613347b77');
+        // $user2->syncRoles('Admin');
+        // $user3->syncRoles('Admin');
+
         // return response()->json($user->getRoleNames()->toArray());
     }
 }
