4c819dac4122842727be25522961951ae0a9d27f
NagaiMatsuge <komilovboburweb@gmail.com>
2020-12-21 17:29:48 +0500

    Paid percentage and projet status are ready

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Models\Project;
+use App\Models\Step;
+use App\Models\Task;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -15,32 +17,7 @@ class ProjectController extends Controller
     //* Fetch all projects with deadline and paid amount in percentage
     public function index(Request $request)
     {
-        $projects = Project::select('projects.*',  DB::raw('(select count(id) from tasks where tasks.project_id = projects.id AND tasks.approved=1) as approved_tasks'), DB::raw('(select count(id) from tasks where tasks.project_id=projects.id) as num_tasks'))->paginate(5)->toArray();
-
-        $tasks = DB::select("select price, debt, project_id from tasks");
-        foreach ($projects['data'] as $key => $project) {
-            $id = $project['id'];
-            $tasksOfProject = [];
-            foreach ($tasks as $task) {
-                if ($task->project_id == $id) {
-                    $tasksOfProject[] = $task;
-                }
-            }
-            $debtPercent = 0;
-            $taskCount = 0;
-            foreach ($tasksOfProject as $t) {
-                $taskCount++;
-                $debtPercent += ($t->price - $t->debt) * 100 / $t->price;
-            }
-            if ($taskCount === 0) {
-                $projects['data'][$key]['paid_in_percentage'] = null;
-            } else {
-                $projects['data'][$key]['paid_in_percentage'] = ($debtPercent * 100) / ($taskCount * 100);
-            }
-            $date = explode("-", $project['deadline']);
-            $projects['data'][$key]['deadline'] = $date[2] . ' ' . config('params.month_format.' . $date[1]) . ' ' .  $date[0];
-        }
-
+        $projects = DB::table('projects')->select('projects.*', DB::raw('(SELECT COUNT(tasks.id) FROM tasks WHERE tasks.step_id IN (SELECT id FROM steps WHERE steps.project_id=projects.id)) as task_count'), DB::raw('(SELECT COUNT(tasks.id) FROM tasks WHERE tasks.step_id IN (SELECT id FROM steps WHERE steps.project_id=projects.id AND tasks.approved=1)) as approved_count'), DB::raw('(SELECT SUM(t1.percent)/ COUNT(t1.percent) AS paid_percent FROM (SELECT ((steps.price - steps.debt) * 100 / steps.price) AS percent FROM steps WHERE steps.project_id=projects.id) AS t1) AS paid_percent'))->paginate(10);
         return $this->successResponse($projects);
     }
 
@@ -97,4 +74,37 @@ class ProjectController extends Controller
         $delete = DB::table('projects')->where('id', $id)->delete();
         return $this->successResponse($delete);
     }
+
+    /*
+public function index(Request $request)
+    {
+        $projects = Project::select('projects.*',  DB::raw('(select count(id) from tasks where tasks.project_id = projects.id AND tasks.approved=1) as approved_tasks'), DB::raw('(select count(id) from tasks where tasks.project_id=projects.id) as num_tasks'))->paginate(5)->toArray();
+
+        $tasks = DB::select("select price, debt, project_id from tasks");
+        foreach ($projects['data'] as $key => $project) {
+            $id = $project['id'];
+            $tasksOfProject = [];
+            foreach ($tasks as $task) {
+                if ($task->project_id == $id) {
+                    $tasksOfProject[] = $task;
+                }
+            }
+            $debtPercent = 0;
+            $taskCount = 0;
+            foreach ($tasksOfProject as $t) {
+                $taskCount++;
+                $debtPercent += ($t->price - $t->debt) * 100 / $t->price;
+            }
+            if ($taskCount === 0) {
+                $projects['data'][$key]['paid_in_percentage'] = null;
+            } else {
+                $projects['data'][$key]['paid_in_percentage'] = ($debtPercent * 100) / ($taskCount * 100);
+            }
+            $date = explode("-", $project['deadline']);
+            $projects['data'][$key]['deadline'] = $date[2] . ' ' . config('params.month_format.' . $date[1]) . ' ' .  $date[0];
+        }
+
+        return $this->successResponse($projects);
+    }
+    */
 }
