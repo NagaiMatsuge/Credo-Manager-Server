ea5c44925c4b9288aa5a48b9adf20347dae228b1
NagaiMatsuge <komilovboburweb@gmail.com>
2020-12-22 11:16:26 +0500

    Step creation is ready

--- app/Http/Controllers/StepController.php
+++ app/Http/Controllers/StepController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Models\Step;
+use App\Models\Task;
 use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Validation\Rule;
 
 class StepController extends Controller
 {
@@ -27,16 +29,22 @@ class StepController extends Controller
     //* Create step with valiadtion
     public function store(Request $request)
     {
-        $create_step = Step::create($request->validate([
-            'title' => 'required|string|min:3|max:255',
-            'project_id' => 'required|integer',
-            'price' => 'required|integer',
-            'debt' => 'required|integer',
-            'currency_id' => 'required|integer',
-            'payment_type' => 'required|integer',
-            'payment_date' => 'required|date|date_format:Y-m-d',
-        ]));
-        return $this->successResponse($create_step);
+        $this->makeValidation($request);
+
+        $step = $request->step;
+        $step['debt'] = $step['price'];
+        $step = Step::create($step);
+
+        if ($request->tasks !== null) {
+            $tasks = $request->tasks;
+
+            foreach ($tasks as $key => $task) {
+                $tasks[$key]['step_id'] = $step->id;
+            }
+
+            Task::updateOrCreate($tasks);
+        }
+        return $this->successResponse([], 201, "Successfully updated");
     }
 
     //* Update step by its id
@@ -51,5 +59,36 @@ class StepController extends Controller
     {
         $delete = DB::table('steps')->where('id', $id)->delete();
         return $this->successResponse($delete);
+    }
+
+    //* Validate requests for Steps
+    public function makeValidation(Request $request)
+    {
+        $request->validate([
+            'step.title' => 'required|string|min:3|max:255',
+            'step.project_id' => 'required|integer',
+            'step.price' => 'required',
+            'step.currency_id' => [
+                'required',
+                Rule::in(array_keys(config('params.currencies')))
+            ],
+            'step.payment_type' => [
+                'required',
+                Rule::in(array_keys(config('params.payment_types')))
+            ],
+            'step.payment_date' => 'required|date|date_format:Y-m-d',
+            'tasks' => 'nullable|array',
+            'tasks.*.title' => [
+                'string',
+                Rule::requiredIf($request->has('tasks')),
+                'min:3',
+                'max:255'
+            ],
+            'tasks.*.deadline' => [
+                Rule::requiredIf($request->has('tasks')),
+                'date',
+                'date_format:Y-m-d'
+            ]
+        ]);
     }
 }
