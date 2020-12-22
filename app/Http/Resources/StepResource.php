3694cc6c0ef9b1ff30fc0c2c1612cd6873258a82
NagaiMatsuge <komilovboburweb@gmail.com>
2020-12-22 17:45:27 +0500

    Added id to step resource

--- app/Http/Resources/StepResource.php
+++ app/Http/Resources/StepResource.php
@@ -15,6 +15,7 @@ class StepResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'title' => $this->title,
             'project_id' => $this->project_id,
             'price' => $this->price,
