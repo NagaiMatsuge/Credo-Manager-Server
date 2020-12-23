819f80ecd2fe35361f445109862faad645f765fa
NagaiMatsuge <komilovboburweb@gmail.com>
2020-12-23 15:29:58 +0500

    Payments routes fix

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -20,9 +20,9 @@ class PaymentController extends Controller
     }
 
     //* Show Payment by its id
-    public function show(Payment $id)
+    public function show(Payment $payment)
     {
-        return $this->successResponse($id);
+        return $this->successResponse($payment);
     }
 
     //* Create Payment with validation
@@ -51,10 +51,9 @@ class PaymentController extends Controller
     }
 
     //* Delete payment by its id
-    public function destroy($id)
+    public function destroy(Payment $payment)
     {
-        DB::transaction(function () use ($id) {
-            $payment = DB::table('payments')->where('id', $id)->first();
+        DB::transaction(function () use ($payment) {
             DB::select('update steps set debt=debt+? where id=?', [$payment->amount, $payment->step_id]);
             $payment->delete();
         });
--- routes/MicroApiRoutes/payments.php
+++ routes/MicroApiRoutes/payments.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'payments'], function () {
     Route::get('/', [PaymentController::class, 'index']);
-    Route::get('/{id}', [PaymentController::class, 'show'])->where(['id' => '[0-9]+']);
+    Route::get('/{payment}', [PaymentController::class, 'show'])->where(['payment' => '[0-9]+']);
     Route::post('/create', [PaymentController::class, 'store']);
-    Route::put('/update/{id}', [PaymentController::class, 'update'])->where(['id' => '[0-9]+']);
-    Route::delete('/delete/{id}', [PaymentController::class, 'destroy'])->where(['id' => '[0-9]+']);
+    Route::put('/update/{payment}', [PaymentController::class, 'update'])->where(['payment' => '[0-9]+']);
+    Route::delete('/delete/{payment}', [PaymentController::class, 'destroy'])->where(['payment' => '[0-9]+']);
 });
