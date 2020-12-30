1e1edc36e82f1ad0e07ecf7083724f9c231494cc
NagaiMatsuge <komilovboburweb@gmail.com>
2020-12-30 15:07:32 +0500

    Seeder fix

--- database/seeders/TaskUserSeeder.php
+++ database/seeders/TaskUserSeeder.php
@@ -20,43 +20,64 @@ class TaskUserSeeder extends Seeder
             'user_id' => $userIds[0],
             'task_id' => 1,
             'active' => true,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 45,
+            'tick' => true,
+            'unlim' => true
         ];
         $taskUser[] = [
             'user_id' => $userIds[0],
             'task_id' => 3,
             'active' => false,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 456,
+            'tick' => false,
+            'unlim' => true
         ];
         $taskUser[] = [
             'user_id' => $userIds[1],
             'task_id' => 4,
             'active' => true,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 375,
+            'tick' => true,
+            'unlim' => false
         ];
         $taskUser[] = [
             'user_id' => $userIds[1],
             'task_id' => 5,
             'active' => false,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 360,
+            'tick' => false,
+            'unlim' => true
         ];
         $taskUser[] = [
             'user_id' => $userIds[2],
             'task_id' => 7,
             'active' => true,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 700,
+            'tick' => false,
+            'unlim' => true
         ];
         $taskUser[] = [
             'user_id' => $userIds[2],
             'task_id' => 8,
             'active' => false,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 455,
+            'tick' => false,
+            'unlim' => false
         ];
         $taskUser[] = [
             'user_id' => $userIds[2],
             'task_id' => 2,
             'active' => false,
-            'created_at' => now()
+            'created_at' => now(),
+            'time' => 600,
+            'tick' => false,
+            'unlim' => true
         ];
         DB::table("task_user")->insert($taskUser);
     }
