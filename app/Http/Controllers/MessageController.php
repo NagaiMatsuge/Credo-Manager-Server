b382f6e3b5f6fc319be08a3cea91184f41c86a0e
NagaiMatsuge <komilovboburweb@gmail.com>
2021-01-06 12:54:00 +0500

    Minor fix

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -35,7 +35,7 @@ class MessageController extends Controller
         });
         $user_ids = DB::table('task_user')->where('task_id', $request->task_id)->get()->pluck('user_id');
         foreach ($user_ids as $user_id) {
-            event(new NewMessage($request->task_id, $request->text, $uploaded_files, $request->user(), $user_id));
+            broadcast(new NewMessage($request->task_id, $request->text, $uploaded_files, $request->user(), $user_id))->toOthers();
         }
 
         return $this->successResponse([$user_ids], 201, 'Successfully created');
