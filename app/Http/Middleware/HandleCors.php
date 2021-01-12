76ae1e473351eb288c59ad80c44f43c997e989e1
themamirof <shohrukh.mamirov@gmail.com>
2021-01-12 10:32:55 +0500

    fix user crud

--- app/Http/Middleware/HandleCors.php
+++ app/Http/Middleware/HandleCors.php
@@ -17,7 +17,7 @@ class HandleCors
     public function handle(Request $request, Closure $next)
     {
         return $next($request)->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS')
+            ->header('Access-Control-Allow-Methods', '*')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     }
 }
