ee6bdb894589a6a6d73c0e615535e5d854e9c327
themamirof <shohrukh.mamirov@gmail.com>
2021-01-20 14:46:51 +0500

    adjust notification controller

--- app/Http/Controllers/NotesController.php
+++ app/Http/Controllers/NotesController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
+use App\Traits\ResponseTrait;
 use Illuminate\Http\Request;
 
 class NotesController extends Controller
 {
-    //
+    use ResponseTrait;
+
+    //* Get all notes
+    public function index(Request $request)
+    {
+        return $this->successResponse(Note::paginate(10));
+    }
+
+    //* Show note by its id
+    public function show(Request $request, $id)
+    {
+        
+    }
 }
--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -5,10 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Norification extends Model
+class Notification extends Model
 {
     use HasFactory;
     protected $fillable = [
-        'text'
+        'user_id',
+        'text',
+        'publish_date'
     ];
 }
