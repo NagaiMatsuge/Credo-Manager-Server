9d8e6f94da0a1cd1d0e29c9f234208bc04bd1b2b
NagaiMatsuge <komilovboburweb@gmail.com>
2021-01-29 15:06:48 +0500

    Minor fix

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -68,7 +68,7 @@ class TaskController extends Controller
                         'time' => $task->time,
                         'type' => $task->type,
                         'unread_count' => $task->unread_count,
-                        'deadline' => $task->deadline,
+                        'deadline' => explode(' ', $task->deadline)[0],
                         'time_spent' => (int)$task->time_spent,
                         'last_time' => (int)$task->additional_time
                     ];
